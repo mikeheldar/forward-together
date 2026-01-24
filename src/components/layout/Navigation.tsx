f68c88d5d88fe78f6370565c327e91bfdae9469f
mikeheldar <michael.helmandarley@gmail.com>
2026-01-24 08:54:07 -0500

    Replace logo with new version and make it bigger in header

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -23,7 +23,7 @@ export function Navigation() {
         <div className="flex justify-between items-center h-16">
           {/* Logo and Header */}
           <Link href="/" className="flex items-center gap-3 md:gap-4 hover:opacity-80 transition-opacity">
-            <div className="relative w-12 h-12 md:w-14 md:h-14 flex-shrink-0">
+            <div className="relative w-16 h-16 md:w-20 md:h-20 flex-shrink-0">
               <Image
                 src="/images/logo.png"
                 alt="Forward Together Logo"
